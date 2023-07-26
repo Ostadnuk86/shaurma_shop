415a771b92d0a9472b321fc31b0847ccf260f1ff
stadnyuk <ostadnuk@bk.ru>
2023-07-26 18:41:36 +0500

    add admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Addresses)
+admin.site.register(Clients)
+admin.site.register(PaymentMethod)
+admin.site.register(Orders)
+admin.site.register(OrderStatus)
+admin.site.register(Items)
+admin.site.register(ItemsOrder)
+admin.site.register(CategoryItems)
